b8c93aaa547d6f06936156e25d5cc488515beff9
Ilya Devder <iladevder@gmail.com>
2025-05-21 20:45:39 +0300

    add: script with initial super user

---
+++ scripts/createAdmin.ts
@@ -0,0 +1,22 @@
+import { createUser } from "@/actions/users";
+import { Role } from "@/generated/prisma";
+async function main() {
+    await createUser(
+        "admin",
+        "admin",
+        "admin",
+        Role.ADMIN,
+        "admin"
+  );
+}
+
+main()
+  .then(() => {
+    console.log("Admin created");
+  })
+  .catch((error) => {
+    console.error(error);
+  })
+  .finally(() => {
+    process.exit(0);
+  });
